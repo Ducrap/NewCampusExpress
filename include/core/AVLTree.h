09d112a14614735411b35d55eea777ca9ca6fd48
CrapDu <du_jiapeng@163.com>
2025-07-03 15:19:23 +0800

    CMakeLists构建

---
+++ include/core/AVLTree.h
@@ -0,0 +1,11 @@
+// include/core/AVLTree.h
+#pragma once
+
+namespace CampusExpress {
+// AVL树管理器接口前置声明
+class IAVLTreeManager {
+public:
+    virtual ~IAVLTreeManager() = default;
+    // 可根据需要添加接口方法声明
+};
+} // namespace CampusExpress
--- include/staff/StaffService.h
+++ include/staff/StaffService.h
@@ -2,6 +2,18 @@
 #pragma once
 #include "../core/User.h"
 #include "../core/Package.h"
+#include <string>
+#include <memory>
+#include <optional>
+#include <vector>
+
+// Forward declaration for dependencies
+namespace CampusExpress {
+struct PackageInfo;
+struct UserInfo;
+class IDatabaseConnector;
+class IAVLTreeManager;
+}
 
 namespace CampusExpress {
 namespace Staff {
@@ -10,13 +22,12 @@ class IStaffService {
 public:
     virtual ~IStaffService() = default;
     
-    // 快递管理
+    // 快递管理接口
     virtual bool addPackage(const PackageInfo& package) = 0;
     virtual bool removePackage(const std::string& trackingNumber) = 0;
-    virtual bool updatePackage(const PackageInfo& package) = 0;
     virtual std::optional<PackageInfo> findPackage(const std::string& trackingNumber) = 0;
     
-    // 用户管理
+    // 用户管理接口
     virtual std::vector<UserInfo> listUsers(int offset, int limit) = 0;
     virtual bool disableUser(const std::string& userId) = 0;
     
